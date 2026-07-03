4d1e0bc11a4cb14fed9e13267d322cbb471136d9
nisa1283 <nisaunal21@istanbularel.edu.tr>
2026-07-03 14:59:40 +0300

    Day 04 - OOP fundamentals with BankAccount class

---
+++ gun4/BankAccount.cs
@@ -0,0 +1,92 @@
+﻿namespace gun4
+{
+    internal class BankAccount
+    {
+        private string hesapNo;
+        private string sahipAdi;
+        private decimal bakiye;
+
+        public BankAccount(string hesapNo, string sahipAdi, decimal bakiye)
+        {
+            this.hesapNo = hesapNo;
+            this.sahipAdi = sahipAdi;
+            if (bakiye < 0)
+            {
+                this.bakiye = 0;
+            }
+            else
+            {
+                this.bakiye = bakiye;
+            }
+        }
+        public void HesapBilgileriniGoster()
+        {
+            Console.WriteLine("Hesap No: " + hesapNo);
+            Console.WriteLine("Sahip Adı: " + sahipAdi);
+            Console.WriteLine("Bakiye: " + bakiye);
+        }
+        public override string ToString()
+        {
+            // Geriye nesnenin bilgilerini içeren anlamlı bir metin döndürüyoruz.
+            return "Hesap No: " + hesapNo + ", Sahip Adı: " + sahipAdi + ", Bakiye: " + bakiye;
+        }
+
+        public void ParaYatir(decimal miktar)
+        {
+            if (miktar <= 0)
+            {
+                Console.WriteLine("Yatırılacak miktar sıfır veya negatif olamaz.");
+                return;
+            }
+            else
+            {
+                bakiye += miktar;
+                Console.WriteLine(miktar + " TL yatırıldı. Yeni bakiye: " + bakiye);
+            }    
+        }
+
+        public void ParaCek(decimal miktar)
+        {
+            if (miktar > 0)
+            {
+                if (miktar <= bakiye)
+                {
+                    bakiye -= miktar;
+                    Console.WriteLine(miktar + " TL çekildi. Yeni bakiye: " + bakiye);
+                }
+                else
+                {
+                    Console.WriteLine("Yetersiz bakiye. İşlem gerçekleştirilemedi.");
+                }
+            }
+            else
+            {
+                 Console.WriteLine("Çekilecek miktar sıfır veya negatif olamaz.");
+            }
+
+        }
+
+        public void ParaTransferi(BankAccount hedefHesap, decimal miktar)
+        {
+            if (miktar > 0)
+            {
+                if (miktar <= bakiye)
+                {
+                    bakiye -= miktar;
+                    hedefHesap.ParaYatir(miktar);
+                    Console.WriteLine(miktar + " TL " + hedefHesap + " hesabına transfer edildi. Yeni bakiye: " + bakiye);
+
+                }
+                else
+                {
+                    Console.WriteLine("Yetersiz bakiye. İşlem gerçekleştirilemedi.");
+                }
+            }
+            else 
+            {
+                Console.WriteLine("Transfer edilecek miktar sıfır veya negatif olamaz.");
+            }
+
+        }
+    }
+}
---
+++ gun4/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace gun4
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount hesap1 = new BankAccount("123456", "ali", 1000);
+            Console.WriteLine(hesap1);
+            hesap1.ParaYatir(500);
+            hesap1.ParaCek(300);
+
+            BankAccount hesap2 = new BankAccount("654321", "veli", 2000);
+            Console.WriteLine(hesap2);
+            hesap2.ParaYatir(1000);
+            hesap2.ParaCek(5000);
+
+            hesap1.ParaTransferi(hesap2,200);
+        }
+    }
+}
