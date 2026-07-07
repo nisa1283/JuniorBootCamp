8207703143bf82b0d7174fb0d802b0c6cb814ec7
nisa1283 <nisaunal21@istanbularel.edu.tr>
2026-07-07 18:13:45 +0300

    feat: SRP ihlali örneği içeren fatura hizmeti eklendi

---
+++ gun8/Invoice.cs
@@ -0,0 +1,20 @@
+﻿namespace gun8
+{
+    public class Invoice
+    {
+        public string CustomerName { get; set; }
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal Quantity { get; set; }
+        public decimal Total { get; set; }
+
+        public Invoice(string customerName, string productName, decimal unitPrice, decimal quantity , decimal total)
+        {
+            this.CustomerName = customerName;
+            this.ProductName = productName;
+            this.UnitPrice = unitPrice;
+            this.Quantity = quantity;
+            this.Total = total;
+        }
+    }
+}
---
+++ gun8/Program.cs
@@ -0,0 +1,31 @@
+﻿using gun8;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Please enter your billing information.");
+        Console.WriteLine("Please enter your name:");
+        string customerName = Console.ReadLine();
+
+        Console.WriteLine("Please enter the product name:");
+        string productName = Console.ReadLine();
+
+        Console.WriteLine("Please enter the unit price:");
+        decimal unitPrice = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Please enter the quantity:");
+        decimal quantity = Convert.ToDecimal(Console.ReadLine());
+
+        Console.WriteLine("Please enter the total:");
+        decimal total = Convert.ToDecimal(Console.ReadLine());
+
+        Invoice invoice = new Invoice(customerName, productName, unitPrice, quantity, total);
+        InvoiceService.CalculateTotal(invoice);
+
+        InvoiceService invoiceService = new InvoiceService();
+        invoiceService.PrintInvoices(invoice);
+        invoiceService.SaveToFile(invoice);
+    }
+}
